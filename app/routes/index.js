fe9ecc77b10bc2ea310c87f7553e21b3ea257cf9
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-03-24 10:21:41 -0700

    Modify question model, add model-getting route to index.js, create each loop on index.hbs to display tiles

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
+//consider modifying to pull questions in reverse order
 export default Ember.Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question')
+    })
+  }
 });
